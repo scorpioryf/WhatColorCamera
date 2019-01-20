6551fac2d218c6f558f677e88a4614ecb284a0ee
yifan <holygangster@163.com>
2019-01-20 14:43:28 +0800

    change words

--- app/src/main/java/com/testdemo/holyg/gittest/MainActivity.java
+++ app/src/main/java/com/testdemo/holyg/gittest/MainActivity.java
@@ -3,7 +3,7 @@ package com.testdemo.holyg.gittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
