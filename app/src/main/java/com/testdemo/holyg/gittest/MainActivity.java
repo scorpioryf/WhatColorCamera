338bf31654473a7b1114f79f86f27b860c1a564c
yifan <holygangster@163.com>
2019-01-21 17:59:55 +0800

    chose mode activity

--- app/src/main/java/com/testdemo/holyg/gittest/MainActivity.java
+++ app/src/main/java/com/testdemo/holyg/gittest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.testdemo.holyg.gittest;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.ActivityCompat;
@@ -17,6 +19,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public  class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE_IMAGE_CAMERA = 1;
+    private static final int REQUEST_CODE_IMAGE_OP = 2;
+    private static final int REQUEST_CODE_OP = 3;
+
     Button picBtn;
     Button camBtn;
 
@@ -39,6 +46,7 @@ public  class MainActivity extends AppCompatActivity implements View.OnClickList
                 Intent intentcam = new Intent();
                 intentcam.setClass(MainActivity.this,Camera.class);
                 startActivity(intentcam);
+                break;
             case R.id.picBtn:
                 new AlertDialog.Builder(this)
                         .setTitle("Plz Chose Mode")
@@ -46,8 +54,22 @@ public  class MainActivity extends AppCompatActivity implements View.OnClickList
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 switch (which){
+                                    case 0:
+                                        Intent getImageByalbum = new Intent(Intent.ACTION_GET_CONTENT);
+                                        getImageByalbum.addCategory(Intent.CATEGORY_OPENABLE);
+                                        getImageByalbum.setType("image/jpeg");
+                                        startActivityForResult(getImageByalbum,REQUEST_CODE_IMAGE_OP);
+                                        break;
+                                        default:;
                                     case 1:
-                                }
+                                        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                                        ContentValues values = new ContentValues(1);
+                                        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                                        Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                                        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                                        startActivityForResult(intent, REQUEST_CODE_IMAGE_CAMERA);
+                                        break;
+                            }
                             }
                         })
                         .show();
